0085f8caf73e825d87672299837612af710d2464
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2024-05-20 11:30:50 +0600

    Adding components for shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+} from "./components/ui/table";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button>Test</Button>
+      <Table>
+        <TableHeader className="bg-slate-100">
+          <TableHead className="border-solid border-2 border-sky-500 ">
+            Country
+          </TableHead>
+          <TableHead>Capital</TableHead>
+          <TableHead>Flag</TableHead>
+          <TableHead>Headline</TableHead>
+          <TableHead>Current Weather</TableHead>
+        </TableHeader>
+        <TableBody>
+          <TableCell>1</TableCell>
+          <TableCell>2</TableCell>
+          <TableCell>3</TableCell>
+          <TableCell>4</TableCell>
+          <TableCell>5</TableCell>
+        </TableBody>
+      </Table>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
